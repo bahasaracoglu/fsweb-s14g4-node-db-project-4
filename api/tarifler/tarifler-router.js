bd3f701fc0dcdcb4bc4d04f8b6feda2b8b167b4c
bahasaracoglu <bahasaracoglu@gmail.com>
2023-05-30 23:49:17 +0300

    tarifler-router

--- api/tarifler/tarifler-router.js
+++ api/tarifler/tarifler-router.js
@@ -1,5 +1,12 @@
 const router = require("express").Router();
-
 const mw = require("./tarifler-middleware");
+
+router.get("/:tarif_id", mw.checkTarifId, (req, res, next) => {
+  try {
+    res.json(req.currentTarif);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
