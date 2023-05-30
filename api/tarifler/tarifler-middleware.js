f5f8a81af976bc82f3bba8873a5b941c7b7ac339
bahasaracoglu <bahasaracoglu@gmail.com>
2023-05-30 23:48:42 +0300

    tarifler-mw

--- api/tarifler/tarifler-middleware.js
+++ api/tarifler/tarifler-middleware.js
@@ -1,1 +1,19 @@
 const tarifModel = require("./tarifler-model");
+
+async function checkTarifId(req, res, next) {
+  try {
+    const isExistTarif = await tarifModel.idyeGoreTarifGetir(
+      req.params.tarif_id
+    );
+    if (isExistTarif.length == 0) {
+      res.status(404).json({ message: "Tarif yok" });
+    } else {
+      req.currentTarif = isExistTarif;
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = { checkTarifId };
