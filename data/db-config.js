22170984c41f8ec53ab31f33fef8f92328b7171c
bahasaracoglu <bahasaracoglu@gmail.com>
2023-05-30 23:50:28 +0300

    db-config

--- data/db-config.js
+++ data/db-config.js
@@ -1,5 +1,5 @@
 const knex = require("knex");
-const NODE_ENV = process.env.NODE_ENV || "development";
-const config = require("../knexfile");
+const configs = require("../knexfile.js");
+const environment = process.env.NODE_ENV || "development";
 
 module.exports = knex(configs[environment]);
